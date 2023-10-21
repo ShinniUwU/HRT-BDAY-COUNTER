9fe544e607d533317f5a8e93ecb910a711e3fc7e
Shinni <69644134+ShinniUwU@users.noreply.github.com>
2023-10-21 10:48:00 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const birthdayTime = document.querySelector('#birthdayTime');
+    const hrtTime = document.querySelector('#hrtTime');
     const countdownContainer = document.querySelector('#countdown');
     const daysCountdown = document.querySelector('#days');
     const hoursCountdown = document.querySelector('#hours');
@@ -10,21 +11,21 @@ document.addEventListener('DOMContentLoaded', function() {
     let yearOfTheEvent = currentTime.getFullYear();
     
     // Set the event date to April 5th
-    let eventDate = new Date(yearOfTheEvent, 3 - 1, 5); // Month is 0-indexed
+    let birthdayDate = new Date(yearOfTheEvent, 3 - 1, 5); // Month is 0-indexed
 
     const isItApril5th = currentTime.getMonth() === 3 - 1 && currentTime.getDate() === 5;
 
-    function countdown() {
+    function birthdayCountdown() {
         const now = new Date();
 
-        if (now > eventDate) {
-            eventDate = new Date(yearOfTheEvent + 1, 3 - 1, 5);
-        } else if (now.getMonth() === 3 - 1 && now.getDate() === 5 && now.getFullYear() === eventDate.getFullYear() + 1) {
-            eventDate = new Date(now.getFullYear(), 3 - 1, 5);
+        if (now > birthdayDate) {
+            birthdayDate = new Date(yearOfTheEvent + 1, 3 - 1, 5);
+        } else if (now.getMonth() === 3 - 1 && now.getDate() === 5 && now.getFullYear() === birthdayDate.getFullYear() + 1) {
+            birthdayDate = new Date(now.getFullYear(), 3 - 1, 5);
         }
 
         const currentTime = now.getTime();
-        const eventTime = eventDate.getTime();
+        const eventTime = birthdayDate.getTime();
         const remainingTime = eventTime - currentTime;
 
         let seconds = Math.floor(remainingTime / 1000);
@@ -37,7 +38,7 @@ document.addEventListener('DOMContentLoaded', function() {
         seconds %= 60;
 
         if (isItApril5th) {
-            console.log('Happy birthday, Shinni!');
+            console.log('Happy birthday!');
             countdownContainer.style.display = "none";
             birthdayTime.style.display = "block";
         } else {
@@ -46,21 +47,21 @@ document.addEventListener('DOMContentLoaded', function() {
             minutesCountdown.textContent = minutes;
             secondsCountdown.textContent = seconds;
 
-            setTimeout(countdown, 1000);
+            setTimeout(birthdayCountdown, 1000);
         }
     }
 
-    countdown();
+    birthdayCountdown();
 
+    // HRT Countdown
+    let hrtStartDate = new Date('10/20/2023');
+    let currentDate = new Date();
+
+    const daysUntilHRT = (date1, date2) => {
+        let difference = date1.getTime() - date2.getTime();
+        let totalDays = Math.ceil(difference / (1000 * 3600 * 24));
+        return totalDays;
+    };
+
+    hrtTime.textContent = daysUntilHRT(hrtStartDate, currentDate) + " days since HRT";
 });
-
-let date_1 = new Date('10/20/2023');
-let date_2 = new Date();
-
-const days = (date_1, date_2) => {
-    let difference = date_1.getTime() - date_2.getTime();
-    let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
-    return TotalDays;
-};
-
-document.getElementById("hrt").innerHTML = days(date_1, date_2) + " days until HRT";
